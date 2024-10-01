c34b517b4e194ac6d7c2271607088c84df4606cc
Yeamin <mdyeaminahamed35@gmail.com>
2024-10-01 09:17:04 +0100

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './Header'
+import Navber from './Navber'
+import Banner from './Banner'
+import Last from './Last'
+import Footer from './Footer'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header/>
+      <Navber/>
+      <Banner/>
+      <Last/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default Layout
